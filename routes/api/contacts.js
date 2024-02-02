d9a1866cb0568bc83288c0d428285e6a36180853
AnetRo <beatrice.ccq@gmail.com>
2024-02-02 16:25:20 +0200

    changes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const contacts = require("../../models/contacts");
+
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json(contacts);
+  res.json({ message: "template message" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
